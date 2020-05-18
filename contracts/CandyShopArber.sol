d15803c27a6dac886239131ff4cec84d5bce19df
vaibhavchellani <vaibhavchellani223@gmail.com>
2020-05-19 02:20:20 +0530

    working testcase

--- contracts/CandyShopArber.sol
+++ contracts/CandyShopArber.sol
@@ -35,25 +35,21 @@ contract CandyShopArber is IUniswapV2Callee {
         address[] memory path = new address[](2);
         uint amountToken;
         uint amountETH;
-
-        // just populates path and amounts
         { // scope for token{0,1}, avoids stack too deep errors
         address token0 = IUniswapV2Pair(msg.sender).token0();
         address token1 = IUniswapV2Pair(msg.sender).token1();
-        require(msg.sender == UniswapV2Library.pairFor(factory, token0, token1),"CandyShop: Sender is not uniswapV2 pair"); // ensure that msg.sender is actually a V2 pair
-        require(amount0 == 0 || amount1 == 0,"CandyShop:One of the amounts should be 0"); // this strategy is unidirectional
+        assert(msg.sender == UniswapV2Library.pairFor(factory, token0, token1)); // ensure that msg.sender is actually a V2 pair
+        assert(amount0 == 0 || amount1 == 0); // this strategy is unidirectional
         path[0] = amount0 == 0 ? token0 : token1;
         path[1] = amount0 == 0 ? token1 : token0;
         amountToken = token0 == address(WETH) ? amount1 : amount0;
         amountETH = token0 == address(WETH) ? amount0 : amount1;
         }
 
-        // one of the tokens should be WETH
-        require(path[0] == address(WETH) || path[1] == address(WETH),"One of the tokens should be WETH"); // this strategy only works with a V2 WETH pair
+        assert(path[0] == address(WETH) || path[1] == address(WETH)); // this strategy only works with a V2 WETH pair
         IERC20 token = IERC20(path[0] == address(WETH) ? path[1] : path[0]);
         IUniswapV1Exchange exchangeV1 = IUniswapV1Exchange(factoryV1.getExchange(address(token))); // get V1 exchange
 
-        // TODO we prob only need the IF condition here
         if (amountToken > 0) {
             (uint minETH) = abi.decode(data, (uint)); // slippage parameter for V1, passed in by caller
             token.approve(address(exchangeV1), amountToken);
@@ -67,17 +63,11 @@ contract CandyShopArber is IUniswapV2Callee {
         } else {
             (uint minTokens) = abi.decode(data, (uint)); // slippage parameter for V1, passed in by caller
             WETH.withdraw(amountETH);
-            // number of tokens we received for swapping ETH to token on uniswapV1
             uint amountReceived = exchangeV1.ethToTokenSwapInput{value: amountETH}(minTokens, uint(-1));
-            // number of tokens we need to return to V2 because of flash swap
             uint amountRequired = UniswapV2Library.getAmountsIn(factory, amountETH, path)[0];
             assert(amountReceived > amountRequired); // fail if we didn't get enough tokens back to repay our flash loan
             assert(token.transfer(msg.sender, amountRequired)); // return tokens to V2 pair
-
-            // TODO: slit profits with sender and lottery, currently giving all profits to lottery
-            assert(token.transfer(address(this), amountReceived - amountRequired)); // keep the rest! (tokens)
-            
-            // TODO swap these tokens to ETH
+            assert(token.transfer(sender, amountReceived - amountRequired)); // keep the rest! (tokens)
         }
     }
     
@@ -89,36 +79,36 @@ contract CandyShopArber is IUniswapV2Callee {
         // calculate arb amount
         uint256 arbAmount = 200000000000000000000;
         IERC20 token = IERC20(tokenAddr);
-
+assert(token.transfer(msg.sender, numTokensObtained));
         // trade num tokens we got from earlier trade back to ETH on uniswapV2
-        address pairAddr = UniswapV2Library.pairFor(factory, address(WETH), tokenAddr);
-        IUniswapV2Pair pair = IUniswapV2Pair(pairAddr);
+        // address pairAddr = UniswapV2Library.pairFor(factory, address(WETH), tokenAddr);
+        // IUniswapV2Pair pair = IUniswapV2Pair(pairAddr);
 
-        require(token.transfer(pairAddr,numTokensObtained),"token transfer was not a success");
+        // require(token.transfer(pairAddr,numTokensObtained),"token transfer was not a success");
 
         // flash loan assets
-        pair.swap(arbAmount,0,msg.sender, '0x');
+        // pair.swap(arbAmount,0,msg.sender, '0x');
     }
 
-    function EthToTokenSwapInputV1Vanilla(address tokenAddr, uint256 min_tokens, uint deadline) public payable {
-        IUniswapV1Exchange exchangeV1 = IUniswapV1Exchange(factoryV1.getExchange(tokenAddr)); // get V1 exchange
-        IERC20 token = IERC20(tokenAddr);
-        uint256 numTokensObtained = exchangeV1.ethToTokenSwapInput{value: msg.value}(min_tokens, uint(-1));
-        assert(token.transfer(msg.sender, numTokensObtained)); // keep the rest! (tokens)
-    }
+    // function EthToTokenSwapInputV1Vanilla(address tokenAddr, uint256 min_tokens, uint deadline) public payable {
+    //     IUniswapV1Exchange exchangeV1 = IUniswapV1Exchange(factoryV1.getExchange(tokenAddr)); // get V1 exchange
+    //     IERC20 token = IERC20(tokenAddr);
+    //     uint256 numTokensObtained = exchangeV1.ethToTokenSwapInput{value: msg.value}(min_tokens, uint(-1));
+    //     assert(token.transfer(msg.sender, numTokensObtained)); // keep the rest! (tokens)
+    // }
 
 
-    function ExactETHForTokensV2(uint amountOutMin, address[] calldata path, address to, uint deadline)
-    external
-    payable
-    returns (uint[] memory amounts){
-        return router01.swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
-    }
+    // function ExactETHForTokensV2(uint amountOutMin, address[] calldata path, address to, uint deadline)
+    // external
+    // payable
+    // returns (uint[] memory amounts){
+    //     return router01.swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
+    // }
 
 
-    function EthToTokenSwapWithBaseSwap(address token,uint amount0Out, uint amount1Out, address to, bytes calldata data) external{
-        address pairAddr = UniswapV2Library.pairFor(factory, address(WETH), token);
-        IUniswapV2Pair pair = IUniswapV2Pair(pairAddr);
-        return pair.swap(amount0Out, amount1Out,to,data);
-    }
+    // function EthToTokenSwapWithBaseSwap(address token,uint amount0Out, uint amount1Out, address to, bytes calldata data) external{
+    //     address pairAddr = UniswapV2Library.pairFor(factory, address(WETH), token);
+    //     IUniswapV2Pair pair = IUniswapV2Pair(pairAddr);
+    //     return pair.swap(amount0Out, amount1Out,to,data);
+    // }
 }
--- test/CandyShopArber.spec.ts
+++ test/CandyShopArber.spec.ts
@@ -1,11 +1,11 @@
 import chai, { expect } from 'chai'
-import { Contract } from 'ethers'
 import { MaxUint256 } from 'ethers/constants'
 import { BigNumber, bigNumberify, defaultAbiCoder, formatEther } from 'ethers/utils'
 import { solidity, MockProvider, createFixtureLoader, deployContract } from 'ethereum-waffle'
 import { expandTo18Decimals } from './shared/utilities'
 import { v2Fixture } from './shared/fixtures'
 import CandyShopArber from '../build/CandyShopArber.json'
+import { Contract } from 'ethers'
 
 chai.use(solidity)
 
@@ -20,9 +20,10 @@ describe('CandyShopArber', () => {
     mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
     gasLimit: 9999999,
   })
+
   const [wallet] = provider.getWallets()
   const loadFixture = createFixtureLoader(provider, [wallet])
-
+  let globalFixtures: any
   let WETH: Contract
   let WETHPartner: Contract
   let WETHExchangeV1: Contract
@@ -43,98 +44,9 @@ describe('CandyShopArber', () => {
       [fixture.factoryV2.address, fixture.factoryV1.address, fixture.router.address],
       overrides
     )
+    globalFixtures = fixture
   })
-  it('EthToTokenSwapInputV1Vanilla', async () => {
-    // add liquidity to V1 at a rate of 1 ETH / 100 X
-    const WETHPartnerAmountV1 = expandTo18Decimals(1000)
-    const ETHAmountV1 = expandTo18Decimals(10)
-    await WETHPartner.approve(WETHExchangeV1.address, WETHPartnerAmountV1)
-    await WETHExchangeV1.addLiquidity(bigNumberify(1), WETHPartnerAmountV1, MaxUint256, {
-      ...overrides,
-      value: ETHAmountV1,
-    })
-
-    const balanceBefore = await provider.getBalance(wallet.address)
-    console.log('CANDYSHOP: ETH holdings of original swapper', balanceBefore.toString())
-    var tokenBalanceBefore = await WETHPartner.balanceOf(wallet.address)
-    console.log('CANDYSHOP: TOKEN holdings of original swapper', tokenBalanceBefore.toString())
-
-    await candyShopArber.EthToTokenSwapInputV1Vanilla(WETHPartner.address, expandTo18Decimals(1), MaxUint256, {
-      ...overrides,
-      value: expandTo18Decimals(1),
-    })
-
-    const balanceAfter = await provider.getBalance(wallet.address)
-    console.log('CANDYSHOP: ETH holdings of original swapper', balanceAfter.toString())
-    console.log('CANDYSHOP: ETH loss', balanceBefore.sub(balanceAfter).toString())
-    var tokenBalanceBefore = await WETHPartner.balanceOf(wallet.address)
-    console.log('CANDYSHOP: TOKEN holdings of original swapper', tokenBalanceBefore.toString())
-  })
-
-  it('ExactETHForTokensV2', async () => {
-    // add liquidity to V2 at a rate of 1 ETH / 200 X
-    const WETHPartnerAmountV2 = expandTo18Decimals(2000)
-    const ETHAmountV2 = expandTo18Decimals(10)
-    await WETHPartner.transfer(WETHPair.address, WETHPartnerAmountV2)
-    await WETH.deposit({ value: ETHAmountV2 })
-    await WETH.transfer(WETHPair.address, ETHAmountV2)
-    await WETHPair.mint(wallet.address, overrides)
-
-    const balanceBefore = await provider.getBalance(wallet.address)
-    console.log('CANDYSHOP: ETH holdings of original swapper', formatEther(balanceBefore))
-    var wethBalanceBefore = await WETH.balanceOf(wallet.address)
-    console.log('CANDYSHOP: WETH TOKEN holdings of original swapper', formatEther(wethBalanceBefore))
-    var tokenBalanceBefore = await WETHPartner.balanceOf(wallet.address)
-    console.log('CANDYSHOP: TOKEN holdings of original swapper', formatEther(tokenBalanceBefore))
-    var swapAmount = expandTo18Decimals(1)
-
-    // I want to swap 1ETH and get 200 tokens from V2
-    await candyShopArber.ExactETHForTokensV2(0, [WETH.address, WETHPartner.address], wallet.address, MaxUint256, {
-      ...overrides,
-      value: swapAmount,
-    })
-
-    const balanceAfter = await provider.getBalance(wallet.address)
-    console.log('CANDYSHOP: ETH holdings of original swapper', formatEther(balanceAfter))
-    var wethBalanceAfter = await WETH.balanceOf(wallet.address)
-    console.log('CANDYSHOP: WETH TOKEN holdings of original swapper', formatEther(wethBalanceAfter))
-    const tokenBalanceAfter = await WETHPartner.balanceOf(wallet.address)
-    console.log('CANDYSHOP: TOKEN holdings of original swapper', formatEther(tokenBalanceAfter))
-    console.log('profit', balanceBefore.sub(balanceAfter).toString())
-  })
-
-  it('ExactETHForTokensV2', async () => {
-    // add liquidity to V2 at a rate of 1 ETH / 200 X
-    const WETHPartnerAmountV2 = expandTo18Decimals(2000)
-    const ETHAmountV2 = expandTo18Decimals(10)
-    await WETHPartner.transfer(WETHPair.address, WETHPartnerAmountV2)
-    await WETH.deposit({ value: ETHAmountV2 })
-    await WETH.transfer(WETHPair.address, ETHAmountV2)
-    await WETHPair.mint(wallet.address, overrides)
-
-    const balanceBefore = await provider.getBalance(wallet.address)
-    console.log('CANDYSHOP: ETH holdings of original swapper', formatEther(balanceBefore))
-    var wethBalanceBefore = await WETH.balanceOf(wallet.address)
-    console.log('CANDYSHOP: WETH TOKEN holdings of original swapper', formatEther(wethBalanceBefore))
-    var tokenBalanceBefore = await WETHPartner.balanceOf(wallet.address)
-    console.log('CANDYSHOP: TOKEN holdings of original swapper', formatEther(tokenBalanceBefore))
-    var swapAmount = expandTo18Decimals(1)
-
-    // I want to swap 1ETH and get 200 tokens from V2
-    await candyShopArber.ExactETHForTokensV2(0, [WETH.address, WETHPartner.address], wallet.address, MaxUint256, {
-      ...overrides,
-      value: swapAmount,
-    })
-
-    const balanceAfter = await provider.getBalance(wallet.address)
-    console.log('CANDYSHOP: ETH holdings of original swapper', formatEther(balanceAfter))
-    var wethBalanceAfter = await WETH.balanceOf(wallet.address)
-    console.log('CANDYSHOP: WETH TOKEN holdings of original swapper', formatEther(wethBalanceAfter))
-    const tokenBalanceAfter = await WETHPartner.balanceOf(wallet.address)
-    console.log('CANDYSHOP: TOKEN holdings of original swapper', formatEther(tokenBalanceAfter))
-    console.log('profit', balanceBefore.sub(balanceAfter).toString())
-  })
-  it('EthToTokenSwapInputV1', async () => {
+  it('CandyShopTokensForEth', async () => {
     // add liquidity to V1 at a rate of 1 ETH / 100 X
     const WETHPartnerAmountV1 = expandTo18Decimals(1000)
     const ETHAmountV1 = expandTo18Decimals(10)
@@ -152,53 +64,45 @@ describe('CandyShopArber', () => {
     await WETH.transfer(WETHPair.address, ETHAmountV2)
     await WETHPair.mint(wallet.address, overrides)
 
-    // const balanceBefore = await provider.getBalance(wallet.address)
-    // console.log('CANDYSHOP: ETH holdings of original swapper', balanceBefore.toString())
-    // const candyShopBalanceBeore = await provider.getBalance(candyShopArber.address)
-    // console.log('CANDYSHOP: ETH holdings before arbitrage:', candyShopBalanceBeore.toString())
+    const balanceBefore = await provider.getBalance(wallet.address)
 
-    // TODO replace with either on-chain function or off-chain function
     // now, execute arbitrage via uniswapV2Call:
     // receive 200 X from V2, get as much ETH from V1 as we can, repay V2 with minimum ETH, keep the rest!
     const arbitrageAmount = expandTo18Decimals(200)
     // instead of being 'hard-coded', the above value could be calculated optimally off-chain. this would be
     // better, but it'd be better yet to calculate the amount at runtime, on-chain. unfortunately, this requires a
     // swap-to-price calculation, which is a little tricky, and out of scope for the moment
-
     const WETHPairToken0 = await WETHPair.token0()
     const amount0 = WETHPairToken0 === WETHPartner.address ? arbitrageAmount : bigNumberify(0)
     const amount1 = WETHPairToken0 === WETHPartner.address ? bigNumberify(0) : arbitrageAmount
-    // console.log('weth partner', WETHPartner.address)
-    var result = await candyShopArber.EthToTokenSwapInputV1Vanilla(
-      WETHPartner.address,
-      expandTo18Decimals(1),
-      MaxUint256,
-      {
-        ...overrides,
-        value: expandTo18Decimals(1),
-      }
+
+    await WETHPair.swap(
+      amount0,
+      amount1,
+      candyShopArber.address,
+      defaultAbiCoder.encode(['uint'], [bigNumberify(1)]),
+      overrides
     )
-    // console.log('result', result)
-    // 0 1000000000000000000 0x8B79F9D5318f74bC8F84eE9B0cc2A077a382d63C 0x0000000000000000000000000000000000000000000000000000000000000001
 
-    // const balanceAfter = await provider.getBalance(wallet.address)
-    // console.log('CANDYSHOP: ETH holdings of original swapper', balanceAfter.toString())
-
-    // const candyShopBalanceAfter = await provider.getBalance(candyShopArber.address)
-    // console.log('CANDYSHOP: ETH holdings after arbitrage:', candyShopBalanceAfter.toString())
-
-    // const profit = balanceAfter.sub(balanceBefore)
-    // const reservesV1 = [
-    //   await WETHPartner.balanceOf(WETHExchangeV1.address),
-    //   await provider.getBalance(WETHExchangeV1.address),
-    // ]
-    // const priceV1 = reservesV1[0].div(reservesV1[1])
-    // const reservesV2 = (await WETHPair.getReserves()).slice(0, 2)
-    // const priceV2 =
-    //   WETHPairToken0 === WETHPartner.address ? reservesV2[0].div(reservesV2[1]) : reservesV2[1].div(reservesV2[0])
-
-    // expect(formatEther(profit)).to.eq('0.548043441089763649') // our profit is ~.5 ETH
-    // expect(priceV1.toString()).to.eq('143') // we pushed the v1 price up to ~143
-    // expect(priceV2.toString()).to.eq('161') // we pushed the v2 price down to ~161
+    const balanceAfter = await provider.getBalance(wallet.address)
+    const profit = balanceAfter.sub(balanceBefore)
+    const reservesV1 = [
+      await WETHPartner.balanceOf(WETHExchangeV1.address),
+      await provider.getBalance(WETHExchangeV1.address),
+    ]
+    const priceV1 = reservesV1[0].div(reservesV1[1])
+    const reservesV2 = (await WETHPair.getReserves()).slice(0, 2)
+    const priceV2 =
+      WETHPairToken0 === WETHPartner.address ? reservesV2[0].div(reservesV2[1]) : reservesV2[1].div(reservesV2[0])
+    expect(formatEther(profit)).to.eq('0.548043441089763649') // our profit is ~.5 ETH
+    expect(priceV1.toString()).to.eq('143') // we pushed the v1 price up to ~143
+    expect(priceV2.toString()).to.eq('161') // we pushed the v2 price down to ~161
   })
 })
+
+async function getAddressHoldings(fixtures: any, provider: any, weth: any, wethPartner: any, userAddress: any) {
+  var ethHoldings = await provider.getBalance(userAddress)
+  var wethHoldings = await fixtures.WETH.balanceOf(weth)
+  var wethPartnerHoldings = await fixtures.WETHPartner.balanceOf(wethPartner)
+  return [formatEther(ethHoldings), formatEther(wethHoldings), formatEther(wethPartnerHoldings)]
+}
